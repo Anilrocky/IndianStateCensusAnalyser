802daa8e828c184b3b8055664ed07daad69988a9
Anilrocky <anilkumarac96@gmail.com>
2023-04-12 15:33:17 +0530

    [Anil] Add:Load Indian States Census Information from a CSV file

--- IndianStateCensusAnalyser/StateCensusAnalyzer.cs
+++ IndianStateCensusAnalyser/StateCensusAnalyzer.cs
@@ -8,6 +8,14 @@ namespace IndianStateCensusAnalyser
     {
         public int ReadStateCensusData(string filePath)
         {
+            if (!File.Exists(filePath))
+                throw new StateCensusException(StateCensusException.ExceptionType.FILE_INCORRECT, "Incorrect file path");
+            if (!filePath.EndsWith(".csv"))
+                throw new StateCensusException(StateCensusException.ExceptionType.TYPE_INCORRECT, "Incorrect file type");
+            var read = File.ReadAllLines(filePath);
+            string header = read[0];
+            if (header.Contains("/"))
+                throw new StateCensusException(StateCensusException.ExceptionType.DELIMETER_INCORRECT, "Incorrect delimeter");
             using (var reader = new StreamReader(filePath))
             using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
@@ -18,6 +26,15 @@ namespace IndianStateCensusAnalyser
                 }
                 return records.Count() - 1;
             }
+        }
+        public bool ReadStateCensusData(string filePath, string actualHeader)
+        {
+            var read = File.ReadAllLines(filePath);
+            string header = read[0];
+            if (header.Equals(actualHeader))
+                return true;
+            else
+                throw new StateCensusException(StateCensusException.ExceptionType.HEADER_INCORRECT, "Incorrect Header");
         }
     }
 }
--- IndianStateCensusAnalyserTest/UnitTest1.cs
+++ IndianStateCensusAnalyserTest/UnitTest1.cs
@@ -5,12 +5,68 @@ namespace IndianStateCensusAnalyserTest
     public class Tests
     {
         public string stateCensusFilePath = @"D:\BridgeLabz\IndianStateCensusAnalyser\IndianStateCensusAnalyserTest\Files\StateCensusData.csv";
+        public string stateCensusTypeFilePath = @"D:\BridgeLabz\IndianStateCensusAnalyser\IndianStateCensusAnalyserTest\Files\StateCensusData.txt";
+        public string stateCensusDelimeterFilePath = @"D:\BridgeLabz\IndianStateCensusAnalyser\IndianStateCensusAnalyserTest\Files\StateCensusDataDelimeter.csv";
+        public string stateCensusHeaderFilePath = @"D:\BridgeLabz\IndianStateCensusAnalyser\IndianStateCensusAnalyserTest\Files\StateCensusDataHeader.csv";
         [Test]
         public void GivenStateCensusData_WhenAnalyzed_ShouldReturn_NoOfRecordsMatches()
         {
             StateCensusAnalyzer analyzer = new StateCensusAnalyzer();
             CSVStateCensus stateCensus = new CSVStateCensus();
             Assert.AreEqual(stateCensus.ReadStateCensusData(stateCensusFilePath), analyzer.ReadStateCensusData(stateCensusFilePath));
+        }
+        [Test]
+        public void GivenStateCensusDataFileIncorrect_WhenAnalyze_ShouldReturn_Exception()
+        {
+            StateCensusAnalyzer analyzer = new StateCensusAnalyzer();
+            try
+            {
+                int records = analyzer.ReadStateCensusData(stateCensusFilePath);
+            }
+            catch (StateCensusException ex)
+            {
+                Assert.AreEqual(ex.Message, "Incorrect file path");
+            }
+        }
+        [Test]
+        public void GivenStateCensusDataFileTypeIncorrect_WhenAnalyze_ShouldReturn_Exception()
+        {
+            StateCensusAnalyzer analyzer = new StateCensusAnalyzer();
+            try
+            {
+                int records = analyzer.ReadStateCensusData(stateCensusTypeFilePath);
+            }
+            catch (StateCensusException ex)
+            {
+                Assert.AreEqual(ex.Message, "Incorrect file type");
+            }
+        }
+        [Test]
+        public void GivenStateCensusDataFileDelimeterIncorrect_WhenAnalyze_ShouldReturn_Exception()
+        {
+            StateCensusAnalyzer analyzer = new StateCensusAnalyzer();
+            try
+            {
+                int records = analyzer.ReadStateCensusData(stateCensusDelimeterFilePath);
+            }
+            catch (StateCensusException ex)
+            {
+                Assert.AreEqual(ex.Message, "Incorrect delimeter");
+            }
+        }
+        [Test]
+        public void GivenStateCensusDataFileHeaderIncorrect_WhenAnalyze_ShouldReturn_Exception()
+        {
+            StateCensusAnalyzer analyzer = new StateCensusAnalyzer();
+            try
+            {
+                bool records = analyzer.ReadStateCensusData(stateCensusHeaderFilePath, "State,Population,AreaInSqKm,DensityPerSqKm");
+                Assert.IsTrue(records);
+            }
+            catch (StateCensusException ex)
+            {
+                Assert.AreEqual(ex.Message, "Incorrect Header");
+            }
         }
     }
 }
